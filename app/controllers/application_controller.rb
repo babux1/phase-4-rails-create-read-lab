53320cd17c5c43054f9274eeac1a189236e1bdf6
Batmunkh Namshir <namshir17@gmail.com>
2022-03-15 09:23:34 -0700

    commit tree b36121aa70cb52fd8f7eae711a5b32cd7bea74ec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # GET /plants
+  def index
+    plants = Plant.all
+    render json: plants
+  end
 
+  # GET /plants/:id
+  def show
+    plant = Plant.find_by(id: params[:id])
+    render json: plant
+  end
+
+  # POST /plants
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
 end
